a318377e1c306008eb8842c14ed9b2f61b2cc323
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-12-31 14:29:46 +0100

    migrate admin

--- eco_data/views.py
+++ eco_data/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def endpoint(request):
-    data = ['default', 'products', 'product/:detail', 'user']
+    data = ['default', 'products', 'product/:detail', 'users', 'user/:detail']
     return Response(data)
 
 @api_view(['GET', 'POST'])
@@ -17,4 +17,12 @@ def products(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def product(request, product_id):
     data = product_id
+    return Response(data)
+
+def users(request):
+    data = ['ade', 'paul', 'gift']
+    return Response(data)
+
+def user_detail(request, user_id):
+    data = user_id
     return Response(data)
