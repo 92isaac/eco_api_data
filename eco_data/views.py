0b93be83a4e8f1460dc314c1503b5bb7f426437f
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-12-31 11:17:29 +0100

    setting up and routes config

--- eco_data/views.py
+++ eco_data/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['GET'])
+def endpoint(request):
+    data = ['default', 'products', 'product/:detail', 'user']
+    return Response(data)
+
+@api_view(['GET', 'POST'])
+def products(request):
+    data = ['phone', 'cloth', 'etc']
+    return Response(data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product(request, product_id):
+    data = product_id
+    return Response(data)
